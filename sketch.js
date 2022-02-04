d33142117b997a9e830212db5e3d47c349fec980
Jeremy-Andrew <97487383+Jeremy-Andrew@users.noreply.github.com>
2022-02-03 22:42:35 -0600

    Add files via upload
    The Future News index+sketch

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let img1;
+
+function preLoad(){
+  img1 = createimg('.. assets/cheese.jpg', 'webflow');
+}
+function setup() {
+  createcanvas(1920,1080);
+  img1.position(700, 900);
+}
+
+</body>
